6745a1b9c188d9027c0696a703fa69bc6f03cdd7
Hydralyncs <39605074+Hydralyncs@users.noreply.github.com>
2019-01-23 15:06:34 -0500

    Add new Depot OpMode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/DepotSideNew.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.opmodes.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.opmodes.AlmondLinear;
+
+@Autonomous(name="Depot Side New",group = "auto")
+public class DepotSideNew extends AlmondLinear {
+
+    public void runOpMode() throws InterruptedException {
+        
+    }
+}
