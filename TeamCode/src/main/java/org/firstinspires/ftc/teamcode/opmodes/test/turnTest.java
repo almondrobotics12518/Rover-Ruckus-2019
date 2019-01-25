3621a08052e732b919d9dc959002becf736862cf
Hydralyncs <39605074+Hydralyncs@users.noreply.github.com>
2019-01-25 13:57:27 -0500

    Add turn test annotation.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/turnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/turnTest.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.opmodes.test;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
 import org.firstinspires.ftc.teamcode.opmodes.AlmondLinear;
 
+@Autonomous(group = "test",name = "Turn Test")
 public class turnTest extends AlmondLinear {
     public void runOpMode(){
         hardwareMap();
